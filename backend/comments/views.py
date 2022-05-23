b59ed724750ccf222f85387e85b41edd3aa633f9
ARyback <adam.ryback@gmail.com>
2022-05-22 21:49:44 -0500

    Edited Get request

--- backend/comments/views.py
+++ backend/comments/views.py
@@ -1,4 +1,3 @@
-from math import perm
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework import status
@@ -9,6 +8,7 @@ from .models import Comment
 # Create your views here.
 
 @api_view(['GET'])
+@permission_classes([AllowAny])
 def get_all_comments(request):
     comments = Comment.objects.all()
     serializer = CommentSerializer(comments, many=True)
