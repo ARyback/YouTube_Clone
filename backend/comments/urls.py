68d4302b41de2e9b8b3eaf07cefdf82f6b260a8f
ARyback <adam.ryback@gmail.com>
2022-05-25 14:06:52 -0500

    Edited urls to accept a string parameter of video_id

--- backend/comments/urls.py
+++ backend/comments/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     # path('all/', views.get_all_comments),
-    path('video_id/', views.get_all_comments_by_video_id),
+    path('<str:video_id>/', views.get_all_comments_by_video_id),
     path('', views.user_comments),
 ]
